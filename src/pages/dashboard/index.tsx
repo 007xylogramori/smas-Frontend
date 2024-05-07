1779ddceaa73446d642976e3729e2f9c819d5c15
Himanshu <xylogramori@gmail.com>
2024-05-07 11:30:52 +0530

    --c

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -38,28 +38,7 @@ const Dashboard = () => {
                 <h2 className="text-white text-sm tracking-wide">
                   {userDetails.email}
                 </h2>
-                <h2 className="flex flex-row items-center justify-center gap-2 hover:text-blue-500 text-blue-700 text-sm cursor-pointer tracking-wide">
-                  <span>change password </span>
-                  <span>
-                    <svg
-                      fill="#1d49c2"
-                      className=""
-                      height="10px"
-                      width="10px"
-                      version="1.1"
-                      id="Capa_1"
-                      xmlns="http://www.w3.org/2000/svg"
-                      xmlnsXlink="http://www.w3.org/1999/xlink"
-                      viewBox="0 0 348.882 348.882"
-                      xmlSpace="preserve"
-                    >
-                      <g>
-                        <path d="M333.988,11.758l-0.42-0.383C325.538,4.04,315.129,0,304.258,0c-12.187,0-23.888,5.159-32.104,14.153L116.803,184.231   c-1.416,1.55-2.49,3.379-3.154,5.37l-18.267,54.762c-2.112,6.331-1.052,13.333,2.835,18.729c3.918,5.438,10.23,8.685,16.886,8.685   c0,0,0.001,0,0.001,0c2.879,0,5.693-0.592,8.362-1.76l52.89-23.138c1.923-0.841,3.648-2.076,5.063-3.626L336.771,73.176   C352.937,55.479,351.69,27.929,333.988,11.758z M130.381,234.247l10.719-32.134l0.904-0.99l20.316,18.556l-0.904,0.99   L130.381,234.247z M314.621,52.943L182.553,197.53l-20.316-18.556L294.305,34.386c2.583-2.828,6.118-4.386,9.954-4.386   c3.365,0,6.588,1.252,9.082,3.53l0.419,0.383C319.244,38.922,319.63,47.459,314.621,52.943z" />
-                        <path d="M303.85,138.388c-8.284,0-15,6.716-15,15v127.347c0,21.034-17.113,38.147-38.147,38.147H68.904   c-21.035,0-38.147-17.113-38.147-38.147V100.413c0-21.034,17.113-38.147,38.147-38.147h131.587c8.284,0,15-6.716,15-15   s-6.716-15-15-15H68.904c-37.577,0-68.147,30.571-68.147,68.147v180.321c0,37.576,30.571,68.147,68.147,68.147h181.798   c37.576,0,68.147-30.571,68.147-68.147V153.388C318.85,145.104,312.134,138.388,303.85,138.388z" />
-                      </g>
-                    </svg>
-                  </span>
-                </h2>
+                
               </div>
             </section>
           </section>
@@ -80,19 +59,11 @@ const Dashboard = () => {
                 History
               </button>
 
-              <button className="w-[90%] md:w-[200px]  hover:bg-red-700 duration-200 transition-all bg-red-500 text-white px-2 py-8 font-bold rounded-md">
-                Announcements
-              </button>
+             
               <a target="_blank" href="https://drive.google.com/file/d/1E1lP1fuKAXD3qJ20QtZRYKytNAiPaedC/view?usp=drivesdk" className="w-[90%] md:w-[200px]  hover:bg-blue-700 duration-200 transition-all bg-blue-500 text-white px-2 py-8 font-bold rounded-md text-center">
                 Explore Menu
               </a>
-              <button disabled className="w-[90%] cursor-not-allowed md:w-[200px] 
-               hover:bg-orange-700 duration-200 transition-all bg-orange-500 text-white px-2 py-8 font-bold rounded-md">
-                Report
-              </button>
-              <button disabled className="w-[90%] cursor-not-allowed md:w-[200px]  hover:bg-purple-700 duration-200 transition-all bg-purple-500 text-white px-2 py-8 font-bold rounded-md">
-                Fee Details
-              </button>
+              
             </div>
           </div>
         </div>
